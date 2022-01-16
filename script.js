15b30f49ca81c5ab73aac0bbf02ae19a05f0ed26
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-16 14:32:39 +0530

    JavaScript Strings as an objects added

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript Strings as Objects
+// x is a string
+let x = "John";
+
+// y is an object
+let y = new String("John");
+
+document.getElementById("myString").innerHTML = 
+typeof x + "<br>" + typeof y;
