1a1bf2dbf2eca38c300b09c4d3b1979d918f19e9
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-16 15:38:16 +0530

    html templates added

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ let text = "a, b, c, d, e";
 const myArray = text.split(",");
 document.getElementById("mySplit").innerHTML = 
 myArray[0] + "<br>" + myArray[1] + "<br>" + myArray[2] + "<br>" + myArray[3] + "<br>" + myArray[4];
+
+
+// HTML Templates
+let header = "Template Literals";
+let tags = ["Template Literals", "Javascript", "ES6"];
+
+let html = `<h2>${header}</h2><ul>`;
+for (const i of tags) {
+    html += `<li>${i}</li>`;
+}
+
+html += `</ul>`;
+document.getElementById("myTemplates").innerHTML = html;
