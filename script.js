872788cb2d19b501f9eaf2fe3138681dcf8c72fb
shubham2o <shubhamsharma.ha@gmail.com>
2022-01-16 14:51:51 +0530

    replace () method added

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ let y = new String("John");
 
 document.getElementById("myString").innerHTML = 
 typeof x + "<br>" + typeof y;
+
+
+// Replace Method
+function myReplace() {
+    let text = document.getElementById("myReplace").innerHTML;
+    document.getElementById("myReplace").innerHTML = text.replace(/MIcRoSoFt/i, "W3Schools");
+}
+
+function undoReplace() {
+    let text = document.getElementById("myReplace").innerHTML;
+    document.getElementById("myReplace").innerHTML = text.replace(/W3sChOoLs/i, "Microsoft");
+}
