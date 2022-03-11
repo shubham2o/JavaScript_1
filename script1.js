79a5c5a5ad7765ed96673bf53dc5091be60b7376
shubham2o <shubhamsharma.ha@gmail.com>
2022-03-11 17:32:44 +0530

    Polyfill for Bind Method via call added

--- script1.js
+++ script1.js
@@ -4,22 +4,62 @@ let name = {
     lastName : "Sharma"
 }
 
-let fullName = function () {
-    document.getElementById( "bind" ).innerHTML = "• " + this.firstName + " " + this.lastName;
+let fullName = function( initials, nationality ) {
+    document.getElementById( "bind" ).innerHTML = 
+    "• " + this.firstName + " " + this.lastName + " " + initials + " " + nationality;
 }
 
-let getFullName = fullName.bind( name );
-getFullName();
+let getFullName = fullName.bind( name , "(SS)" );
+getFullName( "Indian" );
 
 
+
+// Polyfill for Bind Method via Call
+Function.prototype.myBind = function( name, initials ) {
+    let obj = this;
+    return function( nationality ) {
+        obj.call( name , initials, nationality );
+    }
+}
+
+let fullName1 = function( initials, nationality ) {
+    document.getElementById( "poly" ).innerHTML = 
+    "• " + this.firstName + " " + this.lastName + " " + initials + " " + nationality;
+}
+
+let getFullName1 = fullName1.myBind( name , "(SS)" );
+getFullName1( "Indian" );
+
+
+
+// Bind Method
 let gsw = {
     firstName : "Stephen",
     lastName : "Curry"
 }
 
-let fullGsw = function () {
-    document.getElementById( "bind1" ).innerHTML = "• " + this.firstName + " " + this.lastName;
+let fullGsw = function( initials, nationality ) {
+    document.getElementById( "bind1" ).innerHTML = 
+    "• " + this.firstName + " " + this.lastName + " " + initials + " " + nationality;
 }
 
-let getFullGsw = fullGsw.bind(gsw);
-getFullGsw();
+let getFullGsw = fullGsw.bind( gsw , "(SC30)" );
+getFullGsw( "American" );
+
+
+
+// Polyfill for Bind Method via Call
+Function.prototype.myBind1 = function( gsw, initials ) {
+    obj1 = this;
+    return function( nationality ) {
+        obj1.call( gsw, initials, nationality );
+    }
+}
+
+let fullGsw1 = function( initials, nationality ) {
+    document.getElementById( "poly1" ).innerHTML = 
+    "• " + this.firstName + " " + this.lastName + " " + initials + " " + nationality;
+}
+
+let getFullGsw1 = fullGsw1.myBind1( gsw , "(SC30)" );
+getFullGsw1( "American" );
